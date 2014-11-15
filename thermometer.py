60ec0422e78df6bab885f74f58d44cf7606f4643
Mark Ramsey <mramsey2624@gmail.com>
2014-11-15 16:27:00 -0500

    Changing base directory to match the Pi

--- thermometer.py
+++ thermometer.py
@@ -2,7 +2,8 @@ import os
 import glob
 import time
 
-base_dir='/devl/python/'
+#base_dir='/devl/python/'
+base_dir = '/sys/bus/w1/devices/'
 device_folder= glob.glob(base_dir+'28*')[0]
 device_file = device_folder + '/w1_slave'
 print ("Folder..." + device_folder)
